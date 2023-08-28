a3993a731c074acaac2c268847f19139b17eacc3
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-08-28 00:46:42 -0400

    Adjusted html and css. Removed unnecessary IDs and classes and updated ID and class names. Cleaned up code. identified required variables for script.js.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,24 @@
-document.getElementById("shake-button").addEventListener("click", function() {
-    var shakingDiv = document.getElementById("shaking-div");
-    shakingDiv.classList.add("shake-animation");
-    setTimeout(function() {
-      shakingDiv.classList.remove("shake-animation");
-    }, 500); // Duration of animation in milliseconds
-  });
+const apiKey = "ffc69f7838e574f5064cf6010c451746";
+const searchHistoryEl = document.getElementById("search-history-container");
+const fiveDayForecastEl = document.getElementById("weather-forecast");
+const searchInputEl = $("#city-input-form");
+const cityEl = $("#city");
+const currentCityEl = $("#selected-city");
+const DateEl = $("#date-today");
+const WeatherIconEl = $("#weather-icon");
+const temperatureEl = $("#temp");
+const windEl = $("#wind-mph");
+const humidityEl = $("#humidity");
 
-  
+// Add a click event listener to make the 8 ball shake
+$(document).ready(function() {
+    $("body").on("click", function() {
+        $("#shaking-div").addClass("shake");
+
+        // Remove the shake class after the animation completes
+        setTimeout(function() {
+            $("#shaking-div").removeClass("shake");
+        }, 500); // 500ms matches the animation duration in the CSS
+    });
+});
+
